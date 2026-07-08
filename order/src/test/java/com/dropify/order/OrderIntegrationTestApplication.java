21c3d61a1fc083a7a5b5db859236c74dfa6c4c39
Junseung-Ock <junseung0305@kyonggi.ac.kr>
2026-07-08 15:36:54 +0900

    test: order 모듈 통합 테스트 컨텍스트 및 설정 추가

---
+++ order/src/test/java/com/dropify/order/OrderIntegrationTestApplication.java
@@ -0,0 +1,15 @@
+package com.dropify.order;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(
+        scanBasePackages = {"com.dropify.common", "com.dropify.product", "com.dropify.order"},
+        exclude = {KafkaAutoConfiguration.class}
+)
+@EnableJpaRepositories(basePackages = {"com.dropify.common", "com.dropify.product", "com.dropify.order"})
+@EntityScan(basePackages = {"com.dropify.common", "com.dropify.product", "com.dropify.order"})
+public class OrderIntegrationTestApplication {
+}
